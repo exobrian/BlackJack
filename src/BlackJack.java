05bc0b0c463165292e9e0bd9748557b02405beae
Brian Tran <exobrian@yahoo.com>
2022-03-04 12:17:35 -0800

    Working but random bug when showing hand

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -6,17 +7,30 @@ public class BlackJack {
     public static void main(String[] args){
         Deck deck = new Deck();
         Player player = new Player();
+        Computer computer = new Computer();
         Scanner scanner = new Scanner(System.in);
-        player.deal(deck);
 
+        //First Deal
+        player.deal(deck);
         player.showHand();
 
-        while (true){
-            System.out.println("Press h to hit, s to stand. \n Press q to quit: ");
+        //Game Loop
+        loop: while (true){
+            System.out.println("\nPress h to hit, s to stand. \nPress q to quit: ");
             String input = scanner.nextLine();
-            if (input.equals("q")){
-                break;
+            switch (input){
+                case "q":
+                    break loop;
+                case "h":
+                    int j = player.getHand().size();
+                    player.hit(deck);
+                    player.showHand();
+                    break;
+                case "s":
+                    computer.takeTurn();
+                    break loop;
             }
+
             System.out.println(player.getValue());
         }
         //returning the value of your hand
--- src/Player.java
+++ src/Player.java
@@ -1,31 +1,45 @@
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Player{
     @Getter @Setter
-    public Card[] hand = new Card[2];
+    public List<Card> hand = new ArrayList<Card>();
 
     public void deal(Deck deck){
         //Deal Player a hand with two cards drawn from the deck
-        this.getHand()[0] = deck.drawCard();
-        this.getHand()[1] = deck.drawCard();
+        this.getHand().add(deck.drawCard());
+        this.getHand().add(deck.drawCard());
+    }
+
+    public void hit(Deck deck){
+        this.getHand().add(deck.drawCard());
     }
 
     public void showHand(){
+        System.out.println("Player's Hand: ");
         for (Card card : this.hand){
             System.out.println(card.toString());
         }
     }
 
     public int getValue(){
-        int value = 0;
+        int value;
         int sum = 0;
+        int aceCount = 0;
         String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+        String[] royals = {"Jack", "Queen", "King"};
+
+        System.out.println("\nTotal Value: ");
         for (Card card : this.hand){
-            value = Arrays.asList(ranks).indexOf(card.getRank()) + 1;
-            sum = sum + value;
+            if (card.getRank() == "Ace"){
+                aceCount++;
+            }
+        value = Arrays.asList(ranks).indexOf(card.getRank()) > 9 ? 10: Arrays.asList(ranks).indexOf(card.getRank()) + 1;
+        sum = sum + value;
         }
         return sum;
     }
